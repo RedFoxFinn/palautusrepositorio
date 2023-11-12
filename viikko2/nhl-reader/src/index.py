e84e071648f28e8a906a337b9c46560c19e990a3
Antti Aarnio <laborantti.antti@gmail.com>
2023-11-12 21:08:16 +0200

    nhl-reader -> 10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,21 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+    players.sort(key=lambda player: player.get_points(), reverse=True)
+
+    print("Players from FIN\n")
+    for player in players:
+        if player.get_nationality() == "FIN":
+            print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,29 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
-    def __str__(self):
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.penalties = dict['penalties']
+        self.team = dict['team']
+        self.games = dict['games']
+
+    def get_name(self):
         return self.name
+    def get_nationality(self):
+        return self.nationality
+    def get_assists(self):
+        return self.assists
+    def get_goals(self):
+        return self.goals
+    def get_penalties(self):
+        return self.penalties
+    def get_team(self):
+        return self.team
+    def get_games(self):
+        return self.games
+    def get_points(self):
+        return self.goals + self.assists
+
+    def __str__(self):
+        return f"{self.get_name():24}   {self.get_team()}   {self.get_goals()} + {self.get_assists()} = {self.get_points()}"
